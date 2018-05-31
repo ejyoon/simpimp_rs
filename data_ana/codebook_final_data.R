1b6cd08212c26ff0de3620f9098f5e734c7cd900
Erica Yoon <ejyoon@stanford.edu>
2018-05-30 22:14:52 -0700

    changes to codebook

--- data_ana/codebook_final_data.R
+++ data_ana/codebook_final_data.R
@@ -10,9 +10,16 @@ unique_values <- function(variable) {
 d <- read.csv(here("data_ana/data","final_analyzed_data.csv"))
 
 ###### add metadata using attributes 
+attr(d$subid, "label") <- "participant ID"
+attr(d$age_group, "label") <- "age binned"
+attr(d$site, "label") <- "recruitment site"
+attr(d$item_num, "label") <- "number of features"
+attr(d$item_rel, "label") <- "number of features (relative)"
+
 attr(d$trial_type, "shortDescription") <- unique_values(d$trial_type)
 attr(d$item_num, "shortDescription") <- unique_values(d$item_num)
 attr(d$item_rel, "shortDescription") <- unique_values(d$item_rel)
+attr(d$sample, "shortDescription") <- unique_values(d$sample)
 
 #### generate the codebook
 makeCodebook(d, 
